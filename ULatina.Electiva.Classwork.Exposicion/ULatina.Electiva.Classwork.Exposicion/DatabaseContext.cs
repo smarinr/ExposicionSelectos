0a10e16fe112530db7241b29c1c2eb8239dd1680
SERGIO MARIN <sergiomarinrichmond@gmail.com>
2017-07-20 22:38:38 -0600

    Exposicion Topicos Avanzados

---
+++ ULatina.Electiva.Classwork.Exposicion/ULatina.Electiva.Classwork.Exposicion/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULatina.Electiva.Classwork.Exposicion
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext() :
+            base(new SQLiteConnection()
+            {
+                ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = "SQLiteWithEF.db", ForeignKeys = true }.ConnectionString
+            }, true)
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<EmployeeMaster> EmployeeMaster { get; set; }
+    }
+}
---
+++ ULatina.Electiva.Classwork.Exposicion/ULatina.Electiva.Classwork.Exposicion/EmployeeMaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULatina.Electiva.Classwork.Exposicion
+{
+    public class EmployeeMaster
+    {
+        public int ID { get; set; }
+
+        public string EmpName { get; set; }
+
+        public double Salary { get; set; }
+
+        public string Designation { get; set; }
+    }
+}
